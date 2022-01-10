ba129bf62c9701001682572520c418caa81f5d4a
choonghoJo <cjoe731@gmail.com>
2022-01-10 08:28:20 -0500

    today

--- bookmark/models.py
+++ bookmark/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # 데이타를 객체화해서 다룬다.
 # 모델 = 테이블
 # 모델의 필드 = 데이타의 컬럼
-# 인스턴스 : 테이블의 레코드
+# 인스턴스 = 테이블의 레코드
 # 필드의 값(인스턴스의 필드값) = 레코드컬럼 데이타값
 class Bookmark(models.Model):
     site_name = models.CharField(max_length=100)
@@ -14,10 +14,10 @@ class Bookmark(models.Model):
     # 1. d/b 의 column 종류
     # 2. 제약 사항 (몇글자까지)
     # 3. Form의 종류
-    # 4. Form애서 제약 사항
+    # 4. Form에서 제약 사항
     def __str__(self):
         return "이름 : "+self.site_name+", 주소 : "+self.url
 
 # model을 만들었다 => d/b에 어떤 data들을 어떤 형태로 넣을지 결정.
 # migration => d/b에 model의 내용을 반영(tabel생성)
-# makemigrations => model의 변경사항을 추 적해서 기록
+# makemigrations => model의 변경사항을 추적해서 기록
--- bookmark/urls.py
+++ bookmark/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import BookmarkListView, BookmarkCreateView
 
 urlpatterns = [
-    #http://127.0.0.1/bookmark/
-    #
+    # http://127.0.0.1/bookmark/
+    # ???
     path("", BookmarkListView.as_view(), name='list'),
     path("add/", BookmarkCreateView.as_view(), name='add'),
 ]
